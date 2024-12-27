ef7bc20ac279b4202d8c765c4b1171edddf6522e
StudentBI38 <StudentBI38@SYN13ASS-NB-569.alabuga.local>
2024-12-27 10:57:32 +0300

    first commit

---
+++ config/book/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Book
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = '__all__'
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book-list')
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = '__all__'
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book-list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
